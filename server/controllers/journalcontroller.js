0295a98f91be5b1ca0ae8f71bbc30d1690a70843
sarah-reynolds12 <73564744+sarah-reynolds12@users.noreply.github.com>
2021-01-21 07:47:50 -0500

    module 12.3.6

--- server/controllers/journalcontroller.js
+++ server/controllers/journalcontroller.js
@@ -18,6 +18,59 @@ router.post('/create', validateSession, (req, res) => {
     Journal.create(journalEntry)
     .then(journal => res.status(200).json(journal))
     .catch(err => res.status(500).json({error: err}))
-})
+});
+
+//Get ALL entries
+router.get("/", (req, res) => {
+    Journal.findAll()
+    .then(journals => res.status(200).json(journals))
+    .catch(err => res.status(500).json({error: err}))
+});
+
+//Get entries by USER
+router.get("/mine", validateSession, (req, res) => {
+    let userid = req.user.id
+    Journal.findAll({
+        where: {owner: userid}
+    })
+    .then(journals => res.status(200).json(journals))
+    .catch(err => res.status(500).json({error: err}))
+});
+
+//Get entries by TITLE
+router.get('/:title', (req, res) => {
+    let title = req.params.title; 
+
+    Journal.findAll({
+        where: {title: title}
+    })
+    .then(journals => res.status(200).json(journals))
+    .catch(err => res.status(500).json({error: err}))
+});
+
+//Updating entries
+router.put("/update/:entryId", validateSession, (req, res) => {
+    const updateJournalEntry = {
+        title: req.body.journal.title, 
+        date: req.body.journal.date,
+        entry: req.body.journal.entry,
+    };
+
+    const query = { where: { id: req.params.entryId, owner: req.user.id}};
+
+    Journal.update(updateJournalEntry, query)
+    .then((journals) => res.status(200).json(journals))
+    .catch((err) => res.status(500).json({error: err}));
+});
+
+//Deleting an entry
+router.delete("/delete/:id", validateSession, (req, res) => {
+    const query = { where: { id: req.params.id, owner: req.user.id}};
+
+    Journal.destroy(query)
+    .then(() => res.status(200).json({message: "Journal Entry Removed"}))
+    .catch((err) => res.status(500).json({error: err}));
+});
+
 
 module.exports = router;
